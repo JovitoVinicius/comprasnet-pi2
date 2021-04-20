10824a65f018a628834fb60d6ad4cc8fd0bdd818
RayanOvelha <rayanerinan@hotmail.com>
2021-04-19 22:45:33 -0300

    First Commit

---
+++ app/src/main/java/com/siasg/comprasnet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.siasg.comprasnet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
