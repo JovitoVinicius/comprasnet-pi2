0e157aec54bead65ce9032e74a593490712aaeb9
RayanOvelha <rayanerinan@hotmail.com>
2021-05-01 22:40:39 -0300

    att onboarding fragment

--- app/src/main/java/com/siasg/comprasnet/ui/fragment/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/siasg/comprasnet/ui/fragment/onboarding/OnboardingFragment.kt
@@ -5,16 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.siasg.comprasnet.R
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.ListFragment
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.siasg.comprasnet.databinding.FragmentOnboardingBinding
+import com.siasg.comprasnet.ui.adapter.OnboardingAdapter
+import com.siasg.comprasnet.ui.fragment.onboarding.screen.OnboardingFirstFragment
+import com.siasg.comprasnet.ui.fragment.onboarding.screen.OnboardingSecondFragment
+import com.siasg.comprasnet.ui.fragment.onboarding.screen.OnboardingThirdFragment
 
 class OnboardingFragment : Fragment() {
+
+    private lateinit var binding: FragmentOnboardingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+        binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+
+        //criar a fonte de dados
+        val listaFragmentos = arrayListOf(
+            OnboardingFirstFragment(),
+            OnboardingSecondFragment(),
+            OnboardingThirdFragment()
+        )
+
+        // criar o adaptador
+
+        val adaptador = AdaptadorVP(
+            listaFragmentos,
+            requireActivity().supportFragmentManager,
+            lifecycle
+        )
+
+        binding.vpOnboarding.adapter = adaptador
+
+
+        // Inflate the layout for this fragment
+        return binding.root
     }
 
+}
+
+class AdaptadorVP(
+    val listaFragmentos: List<Fragment>,
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle){
+    override fun getItemCount() = listaFragmentos.size
+    override fun createFragment(position: Int) = listaFragmentos[position]
 }
