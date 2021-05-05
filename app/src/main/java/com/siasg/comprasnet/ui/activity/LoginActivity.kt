8bbd84d3bcabf86be661bb4d6b1f653fc7c9d988
biielbezerra <gabrielbezerra38@hotmail.com>
2021-05-05 17:41:20 -0300

    Att Início da Navegação

--- app/src/main/java/com/siasg/comprasnet/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/siasg/comprasnet/ui/activity/LoginActivity.kt
@@ -2,26 +2,17 @@ package com.siasg.comprasnet.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.auth.FirebaseAuth
 import com.siasg.comprasnet.R
-import javax.inject.Inject
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.WithFragmentBindings
 
+@AndroidEntryPoint
+@WithFragmentBindings
 class LoginActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val operacao = auth.signInWithEmailAndPassword("teste@teste.com", "123456")
-        operacao.addOnCompleteListener{resultado ->
-            if(resultado.isSuccessful){
-
-            } else{
-
-            }
-        }
     }
 }
