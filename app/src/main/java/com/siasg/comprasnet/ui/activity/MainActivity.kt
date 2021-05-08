ba9dfe9735daf8c81da6baa079b98f590df1b278
RayanOvelha <rayanerinan@hotmail.com>
2021-05-08 13:07:00 -0300

    barra de navegação clicável e funcionando

--- app/src/main/java/com/siasg/comprasnet/ui/activity/MainActivity.kt
+++ app/src/main/java/com/siasg/comprasnet/ui/activity/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.siasg.comprasnet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.siasg.comprasnet.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,26 +18,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setupViews()
-    }
 
-    fun setupViews() {
-        // Finding the Navigation Controller
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.barNavigationView)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragNavHost) as NavHostFragment
-        val navController = navHostFragment.navController
+        val navController: NavController = navHostFragment.navController
+        setupWithNavController(bottomNavigationView, navController)
 
-        // Setting Navigation Controller with the BottomNavigationView
-        binding.barNavigationView.setupWithNavController(navController)
-
-        // Setting Up ActionBar with Navigation Controller
-        val appBarConfiguration = AppBarConfiguration(
-                topLevelDestinationIds = setOf (
-                        R.id.homeFragment,
-                        R.id.loginFragment,
-                        R.id.mapsFragment,
-                        R.id.moreFragment
-                )
-        )
-        //setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
