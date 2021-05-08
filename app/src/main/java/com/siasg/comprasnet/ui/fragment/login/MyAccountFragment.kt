baee43cab9469764ff9c2756dfcdca2cdde410c4
biielbezerra <gabrielbezerra38@hotmail.com>
2021-05-08 15:13:27 -0300

    Att Fragment my Account

--- app/src/main/java/com/siasg/comprasnet/ui/fragment/login/MyAccountFragment.kt
+++ app/src/main/java/com/siasg/comprasnet/ui/fragment/login/MyAccountFragment.kt
@@ -7,15 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.siasg.comprasnet.R
 
-class LoginFragment : Fragment() {
-
+class MyAccountFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_my_account, container, false)
     }
 
 }
