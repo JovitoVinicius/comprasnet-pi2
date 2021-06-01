d5ddbfe5f421f2cb45a9196fe99ae96943a71c44
RayanOvelha <rayanerinan@hotmail.com>
2021-06-01 20:47:30 -0300

    att telas more e seus data binding, inicio da configuracao de login

--- app/src/main/java/com/siasg/comprasnet/ui/fragment/more/MoreFragment.kt
+++ app/src/main/java/com/siasg/comprasnet/ui/fragment/more/MoreFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.siasg.comprasnet.R
 import com.siasg.comprasnet.databinding.FragmentLoginBinding
 import com.siasg.comprasnet.databinding.FragmentMoreBinding
@@ -12,24 +16,39 @@ import com.siasg.comprasnet.databinding.FragmentMoreBinding
 class MoreFragment : Fragment() {
 
     private lateinit var binding: FragmentMoreBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMoreBinding.inflate(inflater, container, false)
         binding.fragment = this
         binding.lifecycleOwner = this
-        // Inflate the layout for this fragment
+        auth = Firebase.auth
+
         return binding.root
     }
 
-    fun irParaFavoritos(v: View){
+    private fun checarLogin(): Int {
+        val currentUser = auth.currentUser
+        return if (currentUser != null){
+            0
+        } else {
+            findNavController().navigate(R.id.action_moreFragment_to_loginFragment)
+            1
+        }
+    }
 
+
+    fun irParaFavoritos(v: View){
+        if (checarLogin() == 0)
+            findNavController().navigate(R.id.action_moreFragment_to_favoritesFragment)
     }
 
     fun irParaVistos(v: View){
-
+        if (checarLogin() == 0)
+            findNavController().navigate(R.id.action_moreFragment_to_historyFragment)
     }
 
     fun irParaAjuda(v: View){
