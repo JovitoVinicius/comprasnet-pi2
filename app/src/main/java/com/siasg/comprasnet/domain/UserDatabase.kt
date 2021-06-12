650768f484dc172f57151bceae0918077814d89b
biielbezerra <gabrielbezerra38@hotmail.com>
2021-06-12 17:03:27 -0300

    Att integração com o Realtime Database para salvar nome e email do usuário no momento do cadastro.

---
+++ app/src/main/java/com/siasg/comprasnet/domain/UserDatabase.kt
@@ -0,0 +1,3 @@
+package com.siasg.comprasnet.domain
+
+class UserDatabase(val userId: String, val name: String, val email: String)
--- app/src/main/java/com/siasg/comprasnet/ui/fragment/login/SignUpFragment.kt
+++ app/src/main/java/com/siasg/comprasnet/ui/fragment/login/SignUpFragment.kt
@@ -1,6 +1,5 @@
 package com.siasg.comprasnet.ui.fragment.login
 
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,10 +10,12 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.core.Context
 import com.google.firebase.ktx.Firebase
 import com.siasg.comprasnet.R
 import com.siasg.comprasnet.databinding.FragmentSignUpBinding
+import com.siasg.comprasnet.domain.UserDatabase
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -32,8 +33,10 @@ class SignUpFragment : Fragment() {
         binding.fragment = this
         binding.lifecycleOwner = this
         auth = Firebase.auth
+
         return binding.root
     }
+
 
     private fun createAccount(email: String, password: String) {
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
@@ -49,20 +52,38 @@ class SignUpFragment : Fragment() {
 
     @SuppressWarnings
     fun irParaLogin(v: View) {
-        val nome: String = binding.TextEditInputName.text.toString()
-        val email: String = binding.TextEditInputEmail.text.toString()
-        val password1: String = binding.TextEditInputPassword.text.toString()
-        val password2: String = binding.TextEditInputPassword2.text.toString()
+        val nome: String = binding.TextEditInputName.text.toString().trim()
+        val email: String = binding.TextEditInputEmail.text.toString().trim()
+        val password1: String = binding.TextEditInputPassword.text.toString().trim()
+        val password2: String = binding.TextEditInputPassword2.text.toString().trim()
 
         if (email.isEmpty() || nome.isEmpty() || password1.isEmpty() || password2.isEmpty())
             Toast.makeText(context, "Alguns dos campos se encontra vazio", Toast.LENGTH_SHORT).show()
         else {
             if (password1 == password2) {
                 createAccount(email, password2)
+                saveUserDatabase(nome, email)
                 findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
             } else
                 Toast.makeText(context, "As senhas não conferem, digite novamente", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun saveUserDatabase(nome: String, email: String){
+        if(nome.isEmpty()){
+            binding.TextEditInputName.error = "Por favor, digite seu nome"
+            return
+        }
+
+        val ref = FirebaseDatabase.getInstance().getReference("dados")
+        val userId = ref.push().key
+
+        val user = UserDatabase(userId.toString(), nome, email)
+        ref.child(userId.toString()).setValue(user).addOnCompleteListener{
+            Toast.makeText(this.context, "Usuário Salvo com sucesso", Toast.LENGTH_LONG).show()
+        }
+
+
     }
 
 
