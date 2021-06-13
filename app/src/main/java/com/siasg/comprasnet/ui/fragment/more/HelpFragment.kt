c459667217490d50b119199d3893c56d4e040c52
JovitoVinicius <61995014+JovitoVinicius@users.noreply.github.com>
2021-06-12 22:05:18 -0300

    Fragment Chatbot

--- app/src/main/java/com/siasg/comprasnet/ui/fragment/more/HelpFragment.kt
+++ app/src/main/java/com/siasg/comprasnet/ui/fragment/more/HelpFragment.kt
@@ -6,14 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.siasg.comprasnet.R
+import com.siasg.comprasnet.databinding.FragmentHelpBinding
+import com.siasg.comprasnet.databinding.FragmentLoginBinding
 
 class HelpFragment : Fragment() {
+    private lateinit var binding: FragmentHelpBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        return inflater.inflate(R.layout.fragment_help, container, false)
+        binding = FragmentHelpBinding.inflate(inflater, container, false)
+        binding.fragment = this
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
 }
